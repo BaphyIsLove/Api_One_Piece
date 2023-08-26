7d2ba53da70fed0195edbc13263152d6b3a08169
baphy <baphyislove@gmail.com>
2023-08-26 16:31:19 -0600

    complete the bidirectional relations of some entities

--- src/main/java/com/api/onepiece/entity/Occupation.java
+++ src/main/java/com/api/onepiece/entity/Occupation.java
@@ -1,10 +1,13 @@
 package com.api.onepiece.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,5 +24,8 @@ public class Occupation {
 
     @Column(unique = true)
     private String name;
+
+    @OneToMany(mappedBy = "occupation")
+    private List<CharacterEntity> characters;
 
 }
